247e96aeed296482f0be0f78838c962c46867957
leehani <rorean@naver.com>
2022-01-27 13:43:12 +0900

    Add eureka server, client

--- eureka-server/src/main/java/com/example/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/EurekaServerApplication.java
@@ -2,12 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class MemberApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
